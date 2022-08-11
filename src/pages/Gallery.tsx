1c83a72f2f55491c809ed58fc8c84bb1d5ff51a7
junbin123 <540965117@qq.com>
2022-08-11 09:19:21 +0800

    init commit

---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,28 @@
+import { Stage } from 'konva/lib/Stage'
+import type { ReactElement } from 'react'
+
+export default function GalleryPage(): ReactElement {
+	// const { isLoading, isError, error, data } = useQuery('fruits', getFruits)
+	// if (isLoading || isError) {
+	// 	return <LoadingOrError error={error as Error} />
+	// }
+	console.log(Stage)
+
+	return (
+		<div>
+			122
+			{/* <Stage width={window.innerWidth} height={window.innerHeight}> */}
+			{/* <Layer>
+					<Rect
+						x={20}
+						y={50}
+						width={100}
+						height={100}
+						fill='red'
+						shadowBlur={5}
+					/>
+				</Layer> */}
+			{/* </Stage> */}
+		</div>
+	)
+}
